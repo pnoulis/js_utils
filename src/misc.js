db27c206e2362d7435cdd859266262d1509c5993
pnoul <pavlos.noulis@gmail.com>
2023-05-26 16:47:25 +0300

    reformated

--- src/misc.js
+++ src/misc.js
@@ -2,10 +2,10 @@ function flattenObj(obj) {
   const result = {};
 
   for (const i in obj) {
-    if ((typeof obj[i] === 'object') && !Array.isArray(obj[i])) {
+    if (typeof obj[i] === "object" && !Array.isArray(obj[i])) {
       const tmp = flattenObj(obj[i]);
       for (const j in tmp) {
-        result[i + '.' + j] = tmp[j];
+        result[i + "." + j] = tmp[j];
       }
     } else {
       result[i] = obj[i];
@@ -14,6 +14,4 @@ function flattenObj(obj) {
   return result;
 }
 
-export {
-  flattenObj,
-}
+export { flattenObj };
